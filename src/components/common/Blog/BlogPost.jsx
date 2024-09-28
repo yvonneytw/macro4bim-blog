6de25fdc4ea5601c6964e94087873cfeb8ec0dc1
Giuseppe Dotto <gppdtt@gmail.com>
2024-09-28 20:21:57 +0200

    add _redirects

--- src/components/common/Blog/BlogPost.jsx
+++ src/components/common/Blog/BlogPost.jsx
@@ -2,11 +2,13 @@ import PropTypes from "prop-types";
 import PostCategories from "./PostCategories";
 import MarkdownRenderer from "../MarkdownRenderer";
 // import RecentPost from "./components/common/Blog/RecentPost";
-// import PostShareAndMetadata from "./components/common/Blog/PostShareAndMetadata";
+import PostShareAndMetadata from "./PostShareAndMetadata";
 import CommentList from "./CommentList";
 import "./blog.css";
+import { useLocation } from "react-router-dom";
 
 export default function BlogPost({ content }) {
+  const location = useLocation();
   return (
     <>
       <PostCategories />
@@ -14,7 +16,7 @@ export default function BlogPost({ content }) {
         <MarkdownRenderer content={content} />
       </div>
       {/* <RecentPost /> */}
-      {/* <PostShareAndMetadata /> */}
+      <PostShareAndMetadata location={location} likes={1} views={20} date={"2024-08-18"} />
       <CommentList />
     </>
   );
--- src/components/common/Blog/PostShareAndMetadata.jsx
+++ src/components/common/Blog/PostShareAndMetadata.jsx
@@ -1,42 +1,88 @@
-import { useParams } from "react-router-dom";
+import PropTypes from "prop-types";
+import LikeButton from "./LikeButton";
 
-function shareOnLinkedIn({ mdFile }) {
+const linkedInIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+  </svg>
+);
+
+const linkIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z" />
+    <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z" />
+  </svg>
+);
+
+const eyeIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+  </svg>
+);
+
+function shareOnLinkedIn({ location }) {
   return (
     <a
-      // href="https://www.linkedin.com/shareArticle?mini=true&url=https://www.macro4bim.com/post/rename-families-and-their-nested"
-      href={`https://www.linkedin.com/shareArticle?mini=true&url=https://www.macro4bim.com/post/${mdFile}`}
+      href={`https://www.linkedin.com/shareArticle?mini=true&url=https://www.macro4bim.com/post/${location}`}
       target="_blank"
       rel="noopener noreferrer"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        viewBox="0 0 16 16"
-      >
-        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-      </svg>
+      {linkedInIcon}
     </a>
   );
 }
 
-export default function PostShareAndMetadata() {
-  const params = useParams();
-  let mdFile = params.mdFile;
+export default function PostShareAndMetadata({ location, likes, views, date }) {
+  let dateObject = new Date(date);
+
   return (
     <div className="post-metadata">
-      <ul className="social">
-        <li>
-          <shareOnLinkedIn mdFile={mdFile} />
-        </li>
-        <li>🔗</li>
-      </ul>
-      <ul className="categories">
-        {keywords.map((k, i) => (
+      <div>
+        <p>share:</p>
+        {shareOnLinkedIn(location)}
+        <button>{linkIcon}</button>
+      </div>
+      <div>
+        <p>{`date: ${dateObject.toLocaleDateString()}`}</p>
+        <LikeButton likes={likes} />
+        <button>
+          {eyeIcon} <sup>{views}</sup>
+        </button>
+      </div>
+      {/* <ul className="categories">
+        {categories.map((k, i) => (
           <li key={i}>k</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
+
+PostShareAndMetadata.propTypes = {
+  location: PropTypes.string.isRequired,
+  date: PropTypes.string,
+  likes: PropTypes.number,
+  views: PropTypes.number,
+};
+shareOnLinkedIn.propTypes = {
+  location: PropTypes.string,
+};
